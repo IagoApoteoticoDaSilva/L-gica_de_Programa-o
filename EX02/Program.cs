672205b400766bca8c0cc9bb01bf07469e231af8
IAGO SANTOS DO NASCIMENTO <iago.snascimento@senacsp.edu.br>
2025-01-13 19:57:21 -0300

    Aula 13-01

---
+++ EX02/Program.cs
@@ -0,0 +1,14 @@
+﻿//Escreva um prgrama que solicita o nome do usuario e exiba uma mensagem de
+// boas vindas
+
+//Solicitar nome
+
+Console.WriteLine("Qual é seu nome?");
+
+//Lê a entrada do usuario
+
+string nome = Console.ReadLine();
+
+// Exibe uma mensagem de cumprimento
+
+Console.WriteLine("Olá usuário(a) " + nome);
---
+++ EX03/Program.cs
@@ -0,0 +1,18 @@
+﻿//Escrever programa que solicite dois numeros inteiros para o usuario e exiba a soma dos numeros
+
+Console.WriteLine("Digite um numero ");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite outro numero ");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite outro numero de novo ");
+int numero3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite outro numero  de novo mais uma vez ");
+int numero4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("A soma é: " + (numero1 + numero2 + numero3 + numero4));
+
+Console.WriteLine($"A soma é: {numero1 + numero2 + numero3 + numero4} ");
+
